2c980625898c0badc96f23d49b81c8f557e03680
amarchino <marchino.alessandro@gmail.com>
2025-07-28 21:25:55 +0200

    Lesson 184

---
+++ typescript/src/annotations/variables.ts
@@ -0,0 +1,8 @@
+const apples: number = 5;
+const speed: string = 'fast';
+const hasName: boolean = true;
+const nothingMuch: null = null;
+const nothing: undefined = undefined;
+
+// Built-in objects
+const now: Date = new Date();
