814046246626781934038a9c44fbb6d27f465804
amarchino <marchino.alessandro@gmail.com>
2025-07-28 21:03:15 +0200

    Lesson 175

--- typescript/src/fetch-json.ts
+++ typescript/src/fetch-json.ts
@@ -3,5 +3,11 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data;
+  const { id, title, completed } = todo;
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
 });
