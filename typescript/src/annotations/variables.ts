678f9d431a9cd61b73c7ca1f251e28b80a1b4737
amarchino <marchino.alessandro@gmail.com>
2025-07-28 21:28:10 +0200

    Lesson 185

--- typescript/src/annotations/variables.ts
+++ typescript/src/annotations/variables.ts
@@ -6,3 +6,18 @@ const nothing: undefined = undefined;
 
 // Built-in objects
 const now: Date = new Date();
+
+// Array
+let colors: string[] = [ 'red', 'green', 'blue' ];
+let myNumbers: number[] = [ 1, 2, 3 ];
+let thruths: boolean[] = [ true, true, false ];
+
+// Classes
+class Car {}
+let car: Car = new Car();;
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
