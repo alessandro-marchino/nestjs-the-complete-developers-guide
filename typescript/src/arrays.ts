7549bb439664a0be780197720b963b13ab5d02c2
amarchino <marchino.alessandro@gmail.com>
2025-07-29 21:36:55 +0200

    Lesson 200

--- typescript/src/arrays.ts
+++ typescript/src/arrays.ts
@@ -18,3 +18,9 @@ const myCar = carMakers.pop();
 carMakers.map((car: string): string => {
   return car;
 });
+
+// Flexible types
+const importantDates: (Date | string)[] = [ new Date() ];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
+// importantDates.push(100);
