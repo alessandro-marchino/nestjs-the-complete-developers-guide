c6ade8f1879e8d2eba488dbf8260e6276e770214
amarchino <marchino.alessandro@gmail.com>
2025-07-30 23:56:02 +0200

    Lesson 216

--- typescript/src/classes.ts
+++ typescript/src/classes.ts
@@ -5,6 +5,9 @@ class Vehicle {
   }
 }
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
   private drive(): void {
     console.log('vroom');
   }
@@ -14,6 +17,7 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car('orange');
+const car = new Car(4, 'orange');
 car.startDrivingProcess();
 console.log(car.color);
+console.log(car.wheels);
