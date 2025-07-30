b5a9e1926a15da222cb6c08de198b3af75994d5c
amarchino <marchino.alessandro@gmail.com>
2025-07-30 21:58:36 +0200

    Lesson 210

--- typescript/src/interfaces.ts
+++ typescript/src/interfaces.ts
@@ -9,8 +9,18 @@ const oldCivic = {
     return `Name: ${this.name}`;
   }
 };
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary: function() {
+    return `My drink has ${this.sugar} grams of sugar`;
+  }
+};
+
 const printSummary = (item: Reportable): void => {
   console.log(item.summary());
 };
 
 printSummary(oldCivic);
+printSummary(drink);
