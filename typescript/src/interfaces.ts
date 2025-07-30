ba4b297c5bca1cca1cbe82478333e48d40f6b74a
amarchino <marchino.alessandro@gmail.com>
2025-07-30 21:56:45 +0200

    Lesson 209

--- typescript/src/interfaces.ts
+++ typescript/src/interfaces.ts
@@ -1,7 +1,4 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+interface Reportable {
   summary: () => string;
 }
 const oldCivic = {
@@ -12,8 +9,8 @@ const oldCivic = {
     return `Name: ${this.name}`;
   }
 };
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.summary());
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
