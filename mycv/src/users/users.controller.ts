abf31ff3d62612c9fa2d8a3609810e0c6f59bef7
amarchino <marchino.alessandro@gmail.com>
2025-08-02 17:44:24 +0200

    Lesson 51

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto) {
-    console.log(body)
+    const user = await this.usersService.create(body.email, body.password);
+    return user;
   }
 }
