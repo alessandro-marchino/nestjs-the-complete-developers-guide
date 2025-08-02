6bedd407d462f46d7ad21e86d2ddc128632a246b
amarchino <marchino.alessandro@gmail.com>
2025-08-02 21:25:06 +0200

    Lesson 74

--- mycv/src/users/auth.service.ts
+++ mycv/src/users/auth.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
 
+  async signup(email: string, password: string) {
+    // See if email is in use
+    const users = await this.userService.find(email);
+    if(users.length) {
+      throw new BadRequestException('Email in use');
+    }
+    // Hash user password
+    const hashedPassword = await hash(password, 10);
+    // Create a new user and save it
+    // Return the user
+  }
+
+  signin() {}
 }
