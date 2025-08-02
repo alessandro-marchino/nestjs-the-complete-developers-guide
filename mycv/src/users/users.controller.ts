509d2127abe78927a5ac18a83d07630ca2711b7e
amarchino <marchino.alessandro@gmail.com>
2025-08-02 18:07:11 +0200

    Lesson 59

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -7,18 +7,22 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('/signup')
-  async createUser(@Body() body: CreateUserDto) {
-    const user = await this.usersService.create(body.email, body.password);
-    return user;
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.create(body.email, body.password);
   }
 
   @Get('/:id')
-  async findUser(@Param('id') id: string) {
+  findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
   }
 
   @Get()
-  async findAllUsers(@Query('email') email: string) {
+  findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  async removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
