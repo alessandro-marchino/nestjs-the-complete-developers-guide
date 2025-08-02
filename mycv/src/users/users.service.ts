894a04712a3e2ea6917c1e0ef805eaf2b058d7ea
amarchino <marchino.alessandro@gmail.com>
2025-08-02 18:02:02 +0200

    Lesson 57

--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -28,5 +28,11 @@ export class UsersService {
     Object.assign(user, attr);
     return this.repo.save(user);
   }
-  async remove() {}
+  async remove(id: number ) {
+    const user = await this.findOne(id);
+    if(!user) {
+      throw new Error('user not found');
+    }
+    return this.repo.remove(user);
+  }
 }
