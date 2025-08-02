feb8c1b182665054e58c7ee6a45d719072cf3e03
amarchino <marchino.alessandro@gmail.com>
2025-08-02 23:52:01 +0200

    Lesson 86

--- mycv/src/users/interceptors/current-user.interceptor.ts
+++ mycv/src/users/interceptors/current-user.interceptor.ts
@@ -9,7 +9,7 @@ export class CurrentUserInterceptor implements NestInterceptor {
 
   async intercept(context: ExecutionContext, next: CallHandler<any>): Promise<Observable<any>> {
     const request = context.switchToHttp().getRequest();
-    const { userId } = request.session;
+    const { userId } = request.session || {};
     if(userId) {
       const user = await this.usersService.findOne(userId);
       request.currentUser = user;
