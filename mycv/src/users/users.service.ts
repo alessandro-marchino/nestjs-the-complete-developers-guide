4c6c677cb1e6dea8a050e8d4558a71844bd5fd05
amarchino <marchino.alessandro@gmail.com>
2025-08-02 18:35:22 +0200

    Lesson 61

--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -14,7 +14,7 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return this.repo.findOneBy({ id });
   }
   async find(email: string) {
@@ -23,7 +23,7 @@ export class UsersService {
   async update(id: number, attr: Omit<Partial<User>, 'id'>) {
     const user = await this.findOne(id);
     if(!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     if(attr.password) {
       attr.password = await hash(attr.password, 10)
@@ -34,7 +34,7 @@ export class UsersService {
   async remove(id: number ) {
     const user = await this.findOne(id);
     if(!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     return this.repo.remove(user);
   }
