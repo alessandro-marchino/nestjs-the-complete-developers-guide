0abd4ffc3a5712e67f65cff62fbfe3f529653acf
amarchino <marchino.alessandro@gmail.com>
2025-08-03 21:11:14 +0200

    Lesson 128

--- mycv/src/reports/reports.service.ts
+++ mycv/src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto as Partial<Report>);
+    return this.repo.save(report);
+  }
+
+}
