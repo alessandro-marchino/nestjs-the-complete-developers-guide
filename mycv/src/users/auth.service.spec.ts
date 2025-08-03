506637ae059795beb0a82ba5a1b4829d2a745c86
amarchino <marchino.alessandro@gmail.com>
2025-08-03 11:19:32 +0200

    Lesson 95

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -4,34 +4,26 @@ import { UsersService } from './users.service';
 import { User } from './user.entity';
 
 describe('AuthService', () => {
-  // let service: AuthService;
+  let service: AuthService;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [AuthService],
-  //   }).compile();
-
-  //   service = module.get<AuthService>(AuthService);
-  // });
-
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
-
-  it('can create an instance of auth service', async () => {
+  beforeEach(async () => {
     // Create a fake copy of the users service
     const fakeUsersService: Partial<UsersService> = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
     };
-
-    const module = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       providers: [
         AuthService,
         { provide: UsersService, useValue: fakeUsersService }
-      ]
+      ],
     }).compile();
-    const service = module.get(AuthService);
+
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
 });
