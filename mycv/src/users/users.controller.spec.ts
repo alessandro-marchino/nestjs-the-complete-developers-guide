8928035ee9885285981a27f76cb2c733afc36e10
amarchino <marchino.alessandro@gmail.com>
2025-08-03 14:31:10 +0200

    Lesson 109

--- mycv/src/users/users.controller.spec.ts
+++ mycv/src/users/users.controller.spec.ts
@@ -3,18 +3,21 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       findOne: async (id: number) => ({ id, email: 'asdf@asdf.com', password: 'asdf'} as User),
       find: async (email: string) => [ { id: 1, email, password: 'asdf'} as User ],
       // remove: () => {},
       // update: () => {}
     };
-    const fakeAuthService: Partial<AuthService> = {
+    fakeAuthService = {
       // signup: (email: string, password: string) => {},
       // signin: (email: string, password: string) => {}
     };
@@ -31,5 +34,27 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async() => {
+    const users = await controller.findAllUsers('asdf@asdf.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('findUser returns a single user with a given id', async() => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async() => {
+    fakeUsersService.findOne = async () => null;
+    try {
+      await controller.findUser('1');
+    } catch(e) {
+      expect(e).toBeInstanceOf(NotFoundException);
+      return
+    }
+    throw new Error('Should not reach here')
   });
 });
