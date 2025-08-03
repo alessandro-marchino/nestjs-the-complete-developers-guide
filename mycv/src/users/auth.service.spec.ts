c2a3f7de4c6a65e378ac6cccdfa9134e3227c54a
amarchino <marchino.alessandro@gmail.com>
2025-08-03 11:37:10 +0200

    Lesson 98

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -2,13 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
     };
@@ -26,15 +28,26 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 
-  it('creates a new user with a salted and hashed password', async () => {
-    const user = await service.signup('asdf@asdf.com', 'asdf');
-    expect(user.password).not.toEqual('asdf');
-    const [ _, algo, rounds, data ] = user.password.split('$');
-    expect(algo).toEqual('2b');
-    expect(rounds).toEqual('10');
-    const [ salt, hash ] = data.split(/\./);
-    expect(salt).toBeDefined();
-    expect(hash).toBeDefined();
+  // it('creates a new user with a salted and hashed password', async () => {
+  //   const user = await service.signup('asdf@asdf.com', 'asdf');
+  //   expect(user.password).not.toEqual('asdf');
+  //   const [ _, algo, rounds, data ] = user.password.split('$');
+  //   expect(algo).toEqual('2b');
+  //   expect(rounds).toEqual('10');
+  //   const [ salt, hash ] = data.split(/\./);
+  //   expect(salt).toBeDefined();
+  //   expect(hash).toBeDefined();
+  // });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () => Promise.resolve([ { id: 1, email: 'a', password: '1'} as User ]);
+    try {
+      await service.signup('asdf@asdf.com', 'asdf');
+    } catch(e) {
+      expect(e).toBeInstanceOf(BadRequestException);
+      return;
+    }
+    throw new Error('Should not have reached here');
   });
 
 });
