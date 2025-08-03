b7154b98274b057795d07680c55b1c0db8bf8e50
amarchino <marchino.alessandro@gmail.com>
2025-08-03 14:18:24 +0200

    Lesson 106

--- mycv/src/users/users.controller.spec.ts
+++ mycv/src/users/users.controller.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      // findOne: () => {},
+      // find: () => {},
+      // remove: () => {},
+      // update: () => {}
+    };
+    const fakeAuthService: Partial<AuthService> = {
+      // signup: (email: string, password: string) => {},
+      // signin: (email: string, password: string) => {}
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        { provide: UsersService, useValue: fakeUsersService },
+        { provide: AuthService, useValue: fakeAuthService }
+      ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
