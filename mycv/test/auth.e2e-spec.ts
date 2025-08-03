029d8de82bece5f4d20a17c0e96de498a26efe70
amarchino <marchino.alessandro@gmail.com>
2025-08-03 16:56:07 +0200

    Lesson 112

---
+++ mycv/test/auth.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request, { Response } from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from '../src/app.module';
+
+describe('Authentication Syste, (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const signupEmail = 'asdf@asdf.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: signupEmail, password: 'asdf' })
+      .expect(201)
+      .then((res: Response) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(signupEmail)
+      })
+  });
+});
