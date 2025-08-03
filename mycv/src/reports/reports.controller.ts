4bc9c1c660c98575f13557e7c112140d6def4dc9
amarchino <marchino.alessandro@gmail.com>
2025-08-03 21:29:01 +0200

    Lesson 137

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -4,6 +4,8 @@ import { CreateReportDto } from './dto/create-report.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ReportDto } from './dto/report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,6 +13,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   craeteReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
