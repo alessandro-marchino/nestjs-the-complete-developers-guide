96dae3edfb2869fe3159e1a4a6e533b4b4c685a5
amarchino <marchino.alessandro@gmail.com>
2025-08-03 21:19:49 +0200

    Lesson 132

--- mycv/src/users/user.entity.ts
+++ mycv/src/users/user.entity.ts
@@ -1,15 +1,21 @@
 import { Exclude } from 'class-transformer';
-import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Report } from 'src/reports/report.entity';
+import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
+
   @Column()
   @Exclude()
   password: string;
+
+  @OneToMany(() => Report, report => report.user)
+  reports: Report[];
 
   @AfterInsert()
   private logInsert() {
