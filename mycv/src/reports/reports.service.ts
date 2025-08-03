28c54f5a9ae7a048f6db65e6761994c80e65acd0
amarchino <marchino.alessandro@gmail.com>
2025-08-03 21:15:39 +0200

    Lesson 129

--- mycv/src/reports/reports.service.ts
+++ mycv/src/reports/reports.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Report } from './report.entity';
 
 @Injectable()
 export class ReportsService {
